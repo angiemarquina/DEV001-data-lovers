fbabdb4846afb339fd479c257db46288b042d323
Angie Marquina <angiemarquina65@gmail.com>
2022-11-08 08:30:37 -0500

    HU1 terminada + avances con filter

--- src/data.js
+++ src/data.js
@@ -1,9 +1,3 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
+export const filterDirectors = (data, directors) =>{
+  return data.filter(film => film.director === directors)
+}
