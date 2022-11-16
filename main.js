e0a002bb90424efc8346b8f184cb5c5df46bb18b
Angie Marquina <angiemarquina65@gmail.com>
2022-11-16 09:21:15 -0500

    Updates

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-import { filterDirectors } from './data.js';
+import { filterDirectors, sortYears } from './data.js';
 import data from './data/ghibli/ghibli.js';
 
 const safeData = data.films;
 //console.log(safeData)
 
-const allFilms = document.getElementById("showFilms"); 
+const allFilms = document.getElementById("showFilms");
 
 const showData = (films) => {
     films.forEach(film => {
@@ -18,22 +18,33 @@ const showData = (films) => {
         <p class="text-card">Year: ${filmYear}</p>
         <p class="text-card"><i class='bx bxs-star'></i> ${filmScore}</p></div>`
     });
-}
+};
 
 showData(safeData);
 
 const showDirectors = document.querySelector(".select-directors");
 //console.log(showDirectors)
-showDirectors.addEventListener("change",(e) => {
+showDirectors.addEventListener("change", (e) => {
     //console.log(e.target.value)
     let choiceDirector = e.target.value;
-    const pelisDirectors = filterDirectors (safeData,choiceDirector);
+    const byDirector = filterDirectors(safeData, choiceDirector);
     allFilms.innerHTML = "";
-    showData(pelisDirectors);
+    document.getElementById("selectByYears").selectedIndex = 0;
+    showData(byDirector);
 });
 
-const home = document.querySelector(".logo-ghibli")
-home.addEventListener("click",() => {
-    allFilms.innerHTML  = "";
+const showYears = document.querySelector(".select-years")
+showYears.addEventListener("change", (e) => {
+    //console.log(e.target.value)
+    let choiceYear = e.target.value;
+    const byYear = sortYears(safeData, choiceYear);
+    allFilms.innerHTML = "";
+    document.getElementById("selectByDirectors").selectedIndex = 0;
+    showData(byYear);
+});
+
+const home = document.querySelector(".logo-ghibli");
+home.addEventListener("click", () => {
+    allFilms.innerHTML = "";
     showData(safeData);
 });
